201b404bad7bf99d2dbd791ff56d5713d52fcd39
milkfist <alyonazzz12@mail.ru>
2023-11-26 13:46:29 +0300

    JSON and data loading

--- app/src/main/java/com/example/dogpictures/MainActivity.java
+++ app/src/main/java/com/example/dogpictures/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.dogpictures;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String BASE_URL = "https://dog.ceo/api/breeds/image/random";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loadDogImage();
+    }
+
+    private void loadDogImage() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL(BASE_URL);
+                    HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                    InputStream inputStream = httpURLConnection.getInputStream();
+                    InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                    BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                    String result = bufferedReader.readLine();
+                    Log.d("MainActivity1", result);
+                } catch (Exception e) {
+//                    throw new RuntimeException(e);
+                    Log.d("MainActivity1", e.toString());
+                }
+            }
+        }).start();
     }
 }
